d5381632571ba3c8988cb10612cfd57110b54aec
shubham733 <shubhamshelar623@gmail.com>
2022-10-26 23:31:19 +0530

    edited test_run_1.py

--- test_run_1.py
+++ test_run_1.py
@@ -9,22 +9,23 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 # @pytest.mark.parametrize("password", ["StartOfSummer21!",
 #                                              pytest.param("new@new.com", marks=pytest.mark.xfail),
 #                                              pytest.param("new1@gmail.com", marks=pytest.mark.xfail)])
-def test_run_1(playwright: Playwright) -> None:
+def test_run_1(set_up) -> None:
     # Assess - Given
-    browser = playwright.chromium.launch(headless=False, slow_mo=500)
-    context = browser.new_context()
-    context.clear_cookies()
-    # Open new page
-    page = context.new_page()
-    page.goto("https://master.eb.uat.simplelegal.dev/accounts/login/?next=/", timeout=0)
-    page.set_default_timeout(3000)
+    page = set_up
+    # browser = playwright.chromium.launch(headless=False, slow_mo=500)
+    # context = browser.new_context()
+    # context.clear_cookies()
+    # # Open new page
+    # page = context.new_page()
+    # page.goto("https://master.eb.uat.simplelegal.dev/accounts/login/?next=/", timeout=0)
+    # page.set_default_timeout(3000)
 
     # Act - When/And
-    page.locator("input[name=\"username\"]").click()
-    page.locator("input[name=\"username\"]").fill("simplelegaldemo+professional@gmail.com")
-    page.locator("input[name=\"password\"]").click()
-    page.locator("input[name=\"password\"]").fill("StartOfSummer21!")
-    page.locator("button:has-text('Sign In')").click()
+    # page.locator("input[name=\"username\"]").click()
+    # page.locator("input[name=\"username\"]").fill("simplelegaldemo+professional@gmail.com")
+    # page.locator("input[name=\"password\"]").click()
+    # page.locator("input[name=\"password\"]").fill("StartOfSummer21!")
+    # page.locator("button:has-text('Sign In')").click()
     page.locator("[data-testid=\"quick_add\"] svg").click()
     # with page.expect_navigation():
     #     page.locator("text=New Matter").click()
@@ -42,8 +43,8 @@ def test_run_1(playwright: Playwright) -> None:
     # with page.expect_navigation():
     #     page.locator("text=Publish Matter").click()
 
-    context.close()
-    browser.close()
+    # context.close()
+    # browser.close()
 
 #
 # with sync_playwright() as playwright:
