ab6b3f301de9fcabba56cd0f50a81fc5eba47e8a
shubham733 <shubhamshelar623@gmail.com>
2022-10-27 23:33:15 +0530

    added password secrets to yml

--- conftest.py
+++ conftest.py
@@ -1,8 +1,10 @@
+import os
+
 import playwright
 import pytest
 from playwright.sync_api import Playwright
 
-import utils.secret_config
+# import utils.secret_config
 
 
 @pytest.fixture(scope="function")
@@ -16,7 +18,8 @@ def set_up(browser):
     page.goto("https://master.eb.uat.simplelegal.dev/", timeout=0)
     page.set_default_timeout(5000)
     page.fill('input:below(:text("Email"))', "simplelegaldemo+professional@gmail.com")
-    page.fill('input:below(:text("Password"))', utils.secret_config.PASSWORD)
+    # page.fill('input:below(:text("Password"))', utils.secret_config.PASSWORD)
+    page.fill('input:below(:text("Password"))', os.environ['PASSWORD'])
     page.locator("button:has-text('Sign In')").click()
 
     yield page
